aa19252a2a9f109a700c368d6b57ea38354e48e0
无名氏 <33836706+chaojixiaoyao@users.noreply.github.com>
2019-04-20 18:10:36 +0800

    Add files via upload
    实现的功能：
    1.GET and POST
    2.HTML文件的加载
    3.图片的显示
    4.错误页面的响应

--- py_web.py
+++ py_web.py
@@ -1,36 +1,85 @@
 import socket
+import time
 
 
-host = ''
-port = 2000
-
-# s 是一个 socket 实例
-s = socket.socket()
-# s.bind 用于绑定
-s.bind((host, port))
+def log(*args, **kwargs):
+    format = '%Y/%m/%d/%M/%S'
+    value = time.localtime(time.time())
+    t = time.strftime(format, value)
+    print(t, *args, **kwargs)
 
 
-# 用一个无限循环来处理请求
-while True:
-    # 先要 s.listen 开始监听
-    s.listen(5)
-    # 当有客户端过来连接的时候, s.accept 函数就会返回 2 个值
-    # 分别是 连接 和 客户端 ip 地址
-    connection, address = s.accept()
+def error(code=404):
+    e = {
+        404: b'HTTP/1.1 404 error page\r\n\r\n<h1>error page</h1>'
+    }
+    response = e.get(code, b'')
+    return response
 
-    # recv 可以接收客户端发送过来的数据
-    # 参数是要接收的字节数
-    # 返回值是一个 bytes 类型
-    request = connection.recv(1024)
 
-    # bytes 类型调用 decode('utf-8') 来转成一个字符串(str)
-    print('ip and request, {}\n{}'.format(address, request.decode('utf-8')))
+def route_index():
+    headers = 'HTTP/1.1 200 Good\r\nContent-Type:text/html\r\n'
+    # 想显示的都要加入body，写标签放置
+    body = '<h1>hello world</h1><img src="feng.webp">'
+    r = headers + '\r\n' + body
+    # 指定编码格式encode, encoding=设置编码格式
+    return r.encode(encoding='utf-8')
 
-    # b'' 表示这是一个 bytes 对象
-    response = b'HTTP/1.1 200 GOOD\r\n\r\n<h1>Hello World!</h1>'
-    print(response)
-    # 用 sendall 发送给客户端
-    connection.sendall(response)
-    # 发送完毕后, 关闭本次连接
-    connection.close()
+
+def route_image():
+    # 打开图片
+    with open('feng.webp', 'rb')as f:
+        header = b'HTTP/1.1 200 Good\r\nContent-Type: image/webp\r\n'
+        img = header + b'\r\n' + f.read()
+        return img
+
+
+def web_hl():
+    # 打开本地html文件
+    with open('html_show.html', encoding='utf-8') as f:
+        fp = f.read()
+    header = 'HTTP/1.1 200 good\r\nContent-Type: text/html\r\n'
+    r = header + '\r\n' + fp
+    return r.encode(encoding='utf-8')
+
+
+def request_parse(path):
+    li = {
+        '/': route_index,
+        '/feng.webp': route_image,
+        '/hl': web_hl,
+    }
+    response = li.get(path, error)
+    return response()
+
+
+def run(host='', port=2000):
+    with socket.socket() as s:
+        s.bind((host, port))
+        while True:
+            s.listen(5)
+            # content 存储了原始请求
+            content, addres = s.accept()
+            request = content.recv(1024).decode('utf-8')
+            log('request and addres,{}\n{}'.format(request, addres))
+            try:
+                path = request.split()[1]
+                response = request_parse(path)
+                content.sendall(response)
+            except Exception as e:
+                log('error', e)
+                content.close()
+
+
+def main():
+    config = dict(
+        host='',
+        port=2000,
+    )
+    run(**config)
+
+
+if __name__ == '__main__':
+    main()
+
 
